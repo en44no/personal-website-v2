51ed3c62d7fa45da235a8f00b58a49b0f4c7e4c5
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-13 18:35:42 -0300

    Added some styles for Links

--- src/components/Navbar/Links.jsx
+++ src/components/Navbar/Links.jsx
@@ -4,16 +4,45 @@ import { Link } from '@chakra-ui/react';
 const Links = () => {
   return (
     <>
-      <Link mr='1rem' fontWeight='semibold'>
+      <Link
+        mr='1rem'
+        fontWeight='semibold'
+        _hover={{
+          bgGradient: 'linear(to-r, cyan.400, blue.500, purple.600)',
+          bgClip: 'text',
+        }}
+      >
         About
       </Link>
-      <Link mr='1rem' fontWeight='semibold'>
+      <Link
+        mr='1rem'
+        fontWeight='semibold'
+        _hover={{
+          bgGradient: 'linear(to-r, cyan.400, blue.500, purple.600)',
+          bgClip: 'text',
+        }}
+      >
         Contact
       </Link>
-      <Link mr='1rem' fontWeight='semibold'>
+      <Link
+        mr='1rem'
+        fontWeight='semibold'
+        _hover={{
+          bgGradient: 'linear(to-r, cyan.400, blue.500, purple.600)',
+          bgClip: 'text',
+        }}
+      >
         Skills
       </Link>
-      <Link fontWeight='semibold'>Projects</Link>
+      <Link
+        fontWeight='semibold'
+        _hover={{
+          bgGradient: 'linear(to-r, cyan.400, blue.500, purple.600)',
+          bgClip: 'text',
+        }}
+      >
+        Projects
+      </Link>
     </>
   );
 };
