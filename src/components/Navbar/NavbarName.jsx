49f095ffdf31f5d196df89ff7887782e81c34fb1
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-13 19:51:08 -0300

    Separate Navbar name in another component

---
+++ src/components/Navbar/NavbarName.jsx
@@ -0,0 +1,26 @@
+import { Box, Text } from '@chakra-ui/layout';
+import React from 'react';
+
+const NavbarName = () => {
+  return (
+    <>
+      <Box p='4'>
+        <Text
+          fontWeight='bold'
+          bgGradient='linear(to-r, cyan.400, blue.500, purple.500)'
+          bgClip='text'
+          _hover={{
+            transition: 'transform .2s',
+            transform: 'scale(1.06)',
+            ml: '0.5',
+          }}
+          cursor='pointer'
+        >
+          Nahuel Márquez
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default NavbarName;
