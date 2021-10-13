c6ca2950a93a152768754e485c99ae38c4f3026f
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-13 19:49:22 -0300

    Added some styles and effects

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -5,7 +5,18 @@ import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 const Social = () => {
   return (
     <>
-      <Flex alignSelf='flex-start' position='absolute' bottom='5'>
+      <Flex
+        alignSelf='flex-start'
+        position='absolute'
+        bottom='5'
+        borderRadius='9px'
+        boxShadow='md'
+        p='3'
+        _hover={{
+          transition: 'transform .2s',
+          transform: 'scale(1.02)',
+        }}
+      >
         <IconButton mr='1' fontSize='1.5rem'>
           <FaGithub />
         </IconButton>
