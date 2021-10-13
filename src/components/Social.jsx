889bc094cc250f914c3d68912e55f418cc9d08c7
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-13 18:34:05 -0300

    Remove unnecessary container and style props

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { IconButton, Flex, Container } from '@chakra-ui/react';
-import { FaLinkedinIn, FaGithub, FaEnvelope } from 'react-icons/fa';
+import { IconButton, Flex } from '@chakra-ui/react';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 
 const Social = () => {
   return (
     <>
-      <Container maxW='container.xl'>
-        <Flex position='fixed' bottom='5'>
-          <IconButton isRound mr='1' bg='transparent' fontSize='1.5rem'>
-            <FaGithub />
-          </IconButton>
-          <IconButton isRound mr='1' bg='transparent' fontSize='1.5rem'>
-            <FaLinkedinIn />
-          </IconButton>
-          <IconButton isRound bg='transparent' fontSize='1.5rem'>
-            <FaEnvelope />
-          </IconButton>
-        </Flex>
-      </Container>
+      <Flex alignSelf='flex-start' position='absolute' bottom='5'>
+        <IconButton mr='1' fontSize='1.5rem'>
+          <FaGithub />
+        </IconButton>
+        <IconButton mr='1' fontSize='1.5rem'>
+          <FaLinkedin />
+        </IconButton>
+        <IconButton fontSize='1.5rem'>
+          <FaEnvelope />
+        </IconButton>
+      </Flex>
     </>
   );
 };
