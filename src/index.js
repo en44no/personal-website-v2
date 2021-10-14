1b5683e03b4bf7160d363a78ffa359dc92cb2ea1
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-13 23:55:29 -0300

    Added i18next and Suspense

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ColorModeScript } from '@chakra-ui/color-mode';
 import Navbar from './components/Navbar/Navbar';
-
 import theme from './theme';
 import Home from './components/Home/Home';
+import './i18next';
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={theme}>
-      <App />
-      <Navbar />
-      <Home />
+      <Suspense fallback={<h1>.</h1>}>
+        <App />
+        <Navbar />
+        <Home />
+      </Suspense>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
