5b2e04fb923fc7de2e5d915b215c4a5827bfd8ab
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-15 17:32:22 -0300

    Added links, notification and copy mail to clipboard onclick

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { IconButton, Flex } from '@chakra-ui/react';
 import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import { useToast } from '@chakra-ui/react';
 
 const Social = () => {
+  const notification = useToast();
+
+  const notificationAndCopyToClipboard = () => {
+    notification({
+      title: 'Correo electrónico copiado.',
+      description: 'Dirección de Gmail copiada a tu portapapeles.',
+      status: 'info',
+      duration: 4000,
+      position: 'bottom-right',
+      isClosable: true,
+    });
+    navigator.clipboard.writeText('nahuelmarquez12@gmail.com');
+  };
+
   return (
     <>
       <Flex
@@ -17,13 +32,31 @@ const Social = () => {
           transform: 'scale(1.02)',
         }}
       >
-        <IconButton mr='1' fontSize='1.5rem'>
-          <FaGithub />
-        </IconButton>
-        <IconButton mr='1' fontSize='1.5rem'>
-          <FaLinkedin />
-        </IconButton>
-        <IconButton fontSize='1.5rem'>
+        <a
+          href='https://github.com/en44no'
+          target='_blank'
+          rel='noreferrer'
+          title='Clicking here you will be redirected to my GitHub account'
+        >
+          <IconButton mr='1' fontSize='1.5rem'>
+            <FaGithub />
+          </IconButton>
+        </a>
+        <a
+          href='https://www.linkedin.com/in/nahuelmarquez/'
+          target='_blank'
+          rel='noreferrer'
+          title='Clicking here you will be redirected to my Linkedin account'
+        >
+          <IconButton mr='1' fontSize='1.5rem'>
+            <FaLinkedin />
+          </IconButton>
+        </a>
+        <IconButton
+          title='Clicking here you will get my email address'
+          fontSize='1.5rem'
+          onClick={() => notificationAndCopyToClipboard()}
+        >
           <FaEnvelope />
         </IconButton>
       </Flex>
