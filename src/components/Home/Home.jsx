ae42f0a6cb60247f2b0263b52b1b678767003200
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-17 03:21:37 -0300

    Added goToSecondSection button

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Container, Box } from '@chakra-ui/react';
+import { Container, Box, IconButton } from '@chakra-ui/react';
 import Social from '../Social';
 import HomeText from './HomeText';
 import HomeImage from './HomeImage';
+import { FaAngleDoubleDown } from 'react-icons/fa';
+import GoTop from '../GoTop';
 
-const Home = () => {
+const Home = ({ isScrolling }) => {
+  const goToSecondSection = () => {
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <>
       <Container
+        id='home'
         maxW='container.xl'
         display='flex'
         alignItems='center'
         justifyContent='center'
         height='100vh'
+        position='relative'
         width='100%'
         flexDirection='column'
       >
@@ -20,7 +28,23 @@ const Home = () => {
           <HomeText />
           <HomeImage />
         </Box>
-        <Social />
+        <Box alignSelf='flex-start' position='absolute' top='90%'>
+          <Social />
+        </Box>
+        <Box position='absolute' top='90%'>
+          <IconButton
+            onClick={() => {
+              goToSecondSection();
+            }}
+          >
+            <FaAngleDoubleDown fontSize='1.5rem' />
+          </IconButton>
+        </Box>
+        <Box position='absolute' top='90%' right='95' zIndex='100'>
+          <Box position='fixed'>
+            <GoTop isScrolling={isScrolling} />
+          </Box>
+        </Box>
       </Container>
     </>
   );
