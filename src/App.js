8fb88e9f127b71db4446da92a401070e153e01aa
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-17 03:20:15 -0300

    Added components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Projects from './components/Projects/Projects';
+
 function App() {
-  return <div className='App'></div>;
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollHeight(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, [scrollHeight]);
+
+  return (
+    <>
+      <Navbar isScrolling={scrollHeight} />
+      <Home isScrolling={scrollHeight} />
+      <Projects />
+    </>
+  );
 }
 
 export default App;
