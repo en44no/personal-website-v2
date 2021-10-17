4f28458e84fc3ed09e74576fdc8f37d0f18d1375
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-17 11:30:02 -0300

    Added border gradient

--- src/components/Projects/ProjectImage.jsx
+++ src/components/Projects/ProjectImage.jsx
@@ -4,19 +4,29 @@ import { Image, Box } from '@chakra-ui/react';
 const ProjectImage = (props) => {
   return (
     <>
-      <Box display='flex' w='500px' h='250px'>
+      <Box
+        display='flex'
+        w='500px'
+        h='250px'
+        bgGradient='linear(to-r, cyan.400, blue.500, purple.600)'
+        p='0.4%'
+        borderRadius='9px'
+        mt='2'
+        overflow='hidden'
+        boxSizing='content-box'
+        _hover={{
+          transition: 'transform .2s',
+          transform: 'scale(1.02)',
+        }}
+        mb='-2'
+      >
         <Image
-          mt='2'
           src={props.appImage}
           alt={props.appTitle}
           w='500px'
           h='250px'
           borderRadius='9px'
           objectFit='cover'
-          _hover={{
-            transition: 'transform .2s',
-            transform: 'scale(1.02)',
-          }}
         />
       </Box>
     </>
