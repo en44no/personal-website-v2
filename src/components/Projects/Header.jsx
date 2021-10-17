4407a6f39084d63e83334823c555a316fcc85f92
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-17 03:22:40 -0300

    Header titles for projects

---
+++ src/components/Projects/Header.jsx
@@ -0,0 +1,32 @@
+import { Box, Flex, Spacer } from '@chakra-ui/react';
+import React from 'react';
+import CodeAndDemo from './CodeAndDemo';
+
+const Header = (props) => {
+  return (
+    <>
+      <Box>
+        <>
+          <Box
+            mt='1'
+            fontWeight='semibold'
+            as='h4'
+            fontSize='sm'
+            color='#11BAE5'
+          >
+            {props.appType}
+          </Box>
+          <Flex>
+            <Box mt='1' fontWeight='semibold' as='h4' fontSize='xl'>
+              {props.appTitle}
+            </Box>
+            <Spacer />
+            <CodeAndDemo codeLink={props.codeLink} demoLink={props.demoLink} />
+          </Flex>
+        </>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
