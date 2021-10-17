da4411b5fd08ac19b93ae80608e0092a57518398
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-17 03:23:41 -0300

    List of technologies used on project

---
+++ src/components/Projects/Technologies.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Badge } from '@chakra-ui/react';
+
+const Technologies = (props) => {
+  const techs = props.techs;
+
+  return (
+    <>
+      <Box mr='0' mt='2' display='flex'>
+        <>
+          {techs.map((t) => (
+            <a href={t.link} target='_blank' rel='noreferrer' title='Demo'>
+              <Badge
+                borderRadius='full'
+                mr='2'
+                px='2'
+                py='0.5'
+                mt='3'
+                bgGradient='linear(to-r, cyan.400, blue.500, purple.600)'
+                _hover={{
+                  transition: 'transform .2s',
+                  transform: 'scale(1.06)',
+                }}
+                cursor='pointer'
+              >
+                {t.name}
+              </Badge>
+            </a>
+          ))}
+        </>
+      </Box>
+    </>
+  );
+};
+
+export default Technologies;
