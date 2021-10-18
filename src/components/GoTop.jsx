78a824fb240ae337ef53095ee9a06c21f8adfc6d
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-18 19:14:55 -0300

    Refactor GoTop component

--- src/components/GoTop.jsx
+++ src/components/GoTop.jsx
@@ -4,18 +4,26 @@ import { UilArrowUp } from '@iconscout/react-unicons';
 
 const GoTop = ({ isScrolling }) => {
   const goToTop = () => {
-    document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
   return (
     <>
-      <Container maxW='container.xl' position='relative'>
-        {isScrolling > 120 ? (
-          <Box onClick={() => goToTop()}>
-            <IconButton bg='#11BAE5'>
+      <Container maxW='container.xl'>
+        <Box display='block' float='right' mr='4.1rem'>
+          {isScrolling > 100 ? (
+            <IconButton
+              className='goTop'
+              onClick={() => goToTop()}
+              zIndex='100'
+              bg='#11BAE5'
+              top='90%'
+              position='fixed'
+              cursor='pointer'
+            >
               <UilArrowUp size='1.8rem' />
             </IconButton>
-          </Box>
-        ) : null}
+          ) : null}
+        </Box>
       </Container>
     </>
   );
