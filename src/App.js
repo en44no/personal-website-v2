a79714ecb3cee71640cbab25e17c40135902f774
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-18 19:28:19 -0300

    Removed GoTop functions from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
@@ -9,29 +9,18 @@ import 'aos/dist/aos.css';
 import GoTop from './components/GoTop';
 
 function App() {
-  const [scrollHeight, setScrollHeight] = useState(0);
-
-  const handleScroll = () => {
-    const position = window.pageYOffset;
-    setScrollHeight(position);
-  };
-
   useEffect(() => {
     Aos.init({
       mirror: true,
     });
   });
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-  }, [scrollHeight]);
-
   return (
     <>
       <div data-aos='fade-down' data-aos-duration='600'>
-        <Navbar isScrolling={scrollHeight} />
-        <GoTop isScrolling={scrollHeight} />
-        <Home isScrolling={scrollHeight} />
+        <Navbar />
+        <GoTop />
+        <Home />
         <Projects />
         {/* <About /> */}
       </div>
