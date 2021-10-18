e74381d851b8e7516023b845f1c947734480719e
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-17 23:59:25 -0300

    Removed title

--- src/components/Projects/MoreProjectsButton.jsx
+++ src/components/Projects/MoreProjectsButton.jsx
@@ -9,7 +9,6 @@ const MoreProjectsButton = () => {
         href='https://github.com/en44no?tab=repositories'
         target='_blank'
         rel='noreferrer'
-        title='GitHub Projects'
       >
         <Button leftIcon={<UilGithubAlt size='1.4rem' />} bg='#11BAE5'>
           Ver más proyectos
