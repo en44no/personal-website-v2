0db3bb0a5f3b6e3712bdc22fc5ddb630e50c8e70
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-18 22:36:04 -0300

    Removed aos animations

--- src/App.js
+++ src/App.js
@@ -4,26 +4,16 @@ import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import './index.css';
 import Projects from './components/Projects/Projects';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
 import GoTop from './components/GoTop';
 
 function App() {
-  useEffect(() => {
-    Aos.init({
-      mirror: true,
-    });
-  });
-
   return (
     <>
-      <div data-aos='fade-down' data-aos-duration='600'>
-        <Navbar />
-        <GoTop />
-        <Home />
-        <Projects />
-        {/* <About /> */}
-      </div>
+      <Navbar />
+      <GoTop />
+      <Home />
+      <Projects />
+      {/* <About /> */}
     </>
   );
 }
