38caec4d895a2f00e3e175e24f791d13980bd982
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-19 21:34:10 -0300

    Added About info

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { Container, Text } from '@chakra-ui/react';
+import { Container, Text, Grid, Box } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
+import {
+  UilLocationPoint,
+  UilEnglishToChinese,
+} from '@iconscout/react-unicons';
+import Resume from '../Home/Resume';
+import GoNextSection from '../GoNextSection';
+import Social from '../Social';
 
 const About = () => {
   const { t } = useTranslation();
@@ -27,9 +34,68 @@ const About = () => {
         >
           {t('AboutMe.1')}
         </Text>
-        <Text fontWeight='semibold' fontSize='xl' textAlign='center' p='20'>
-          Text here
+        <Text position='absolute' top='20%' fontWeight='semibold' fontSize='lg'>
+          {t('FrontEndDevJr.1')}
         </Text>
+        <Box w='90%'>
+          <Grid templateColumns='repeat(2, 1fr)' gap={6}>
+            <Box alignSelf='center'>
+              <Text
+                fontWeight='semibold'
+                fontSize='xl'
+                textAlign='center'
+                style={{ whiteSpace: 'pre-line' }}
+              >
+                {t('AboutMeText.1')}
+              </Text>
+            </Box>
+            <Box alignSelf='center'>
+              <Text
+                fontWeight='semibold'
+                fontSize='xl'
+                textAlign='center'
+                justifyContent='center'
+                display='flex'
+              >
+                <span
+                  style={{
+                    marginRight: '0.5rem',
+                    marginTop: '0.2rem',
+                  }}
+                >
+                  <UilLocationPoint />
+                </span>
+                Piriápolis, Uruguay
+              </Text>
+              <Text
+                fontWeight='semibold'
+                fontSize='xl'
+                textAlign='center'
+                display='flex'
+                justifyContent='center'
+                mt='2'
+              >
+                <span style={{ marginRight: '0.5rem', marginTop: '0.2rem' }}>
+                  <UilEnglishToChinese />
+                </span>
+                Spanish - English
+              </Text>
+              <Box
+                display='flex'
+                justifyContent='center'
+                px='9.5rem'
+                ml='2'
+                mt='2'
+              >
+                <Social />
+              </Box>
+              <Box textAlign='center' px='9.5rem' ml='2'>
+                <Resume />
+              </Box>
+            </Box>
+          </Grid>
+        </Box>
+        <GoNextSection section='projects' />
       </Container>
     </>
   );
