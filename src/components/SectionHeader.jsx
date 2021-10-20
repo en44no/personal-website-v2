397c7df3381db366c90f2edd5e04e6f39722c56d
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-19 23:00:54 -0300

    Separate the title of the sections in a component

---
+++ src/components/SectionHeader.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Text } from '@chakra-ui/react';
+
+const SectionHeader = (props) => {
+  return (
+    <>
+      <Text
+        position='absolute'
+        top='13%'
+        fontWeight='bold'
+        fontSize='2xl'
+        bgGradient='linear(to-r, cyan.400, blue.500, purple.600)'
+        bgClip='text'
+      >
+        {props.title}
+      </Text>
+    </>
+  );
+};
+
+export default SectionHeader;
