5e0ce63e4b3e8371933fddeeabc424e6310c3ce7
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-19 23:11:08 -0300

    Separated About in others components

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Container, Text, Grid, Box } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
-import {
-  UilLocationPoint,
-  UilEnglishToChinese,
-} from '@iconscout/react-unicons';
-import Resume from '../Home/Resume';
 import GoNextSection from '../GoNextSection';
-import Social from '../Social';
 import SectionHeader from '../SectionHeader';
+import Content from './Content';
+import SectionSubtitle from '../SectionSubtitle';
 
 const About = () => {
   const { t } = useTranslation();
@@ -26,67 +22,8 @@ const About = () => {
         flexDirection='column'
       >
         <SectionHeader title={t('AboutMe.1')} />
-        <Text position='absolute' top='20%' fontWeight='semibold' fontSize='lg'>
-          {t('FrontEndDevJr.1')}
-        </Text>
-        <Box w='90%'>
-          <Grid templateColumns='repeat(2, 1fr)' gap={6} display='flex'>
-            <Box alignSelf='center'>
-              <Text
-                fontWeight='semibold'
-                fontSize='xl'
-                textAlign='center'
-                style={{ whiteSpace: 'pre-line' }}
-              >
-                {t('AboutMeText.1')}
-              </Text>
-            </Box>
-            <Box alignSelf='center'>
-              <Text
-                fontWeight='semibold'
-                fontSize='xl'
-                textAlign='center'
-                justifyContent='center'
-                display='flex'
-              >
-                <span
-                  style={{
-                    marginRight: '0.5rem',
-                    marginTop: '0.2rem',
-                  }}
-                >
-                  <UilLocationPoint />
-                </span>
-                Piriápolis, Uruguay
-              </Text>
-              <Text
-                fontWeight='semibold'
-                fontSize='xl'
-                textAlign='center'
-                display='flex'
-                justifyContent='center'
-                mt='2'
-              >
-                <span style={{ marginRight: '0.5rem', marginTop: '0.2rem' }}>
-                  <UilEnglishToChinese />
-                </span>
-                Spanish - English
-              </Text>
-              <Box
-                display='flex'
-                justifyContent='center'
-                px='9.5rem'
-                ml='2'
-                mt='2'
-              >
-                <Social />
-              </Box>
-              <Box ml='2' px='7rem'>
-                <Resume />
-              </Box>
-            </Box>
-          </Grid>
-        </Box>
+        <SectionSubtitle subtitle={t('FrontEndDevJr.1')} />
+        <Content />
         <GoNextSection section='projects' />
       </Container>
     </>
