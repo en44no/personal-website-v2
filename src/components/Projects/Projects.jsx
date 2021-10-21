f911421cd66b46a710d17650c0d073ec45161372
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-21 18:11:37 -0300

    Using SectionTemplate component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,42 +1,30 @@
-import { Container, Box } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import List from './List';
 import MoreProjectsButton from './MoreProjectsButton';
-import SectionNextButton from '../Section/SectionNextButton';
-import SectionHeader from '../Section/SectionHeader';
-import SectionSubtitle from '../Section/SectionSubtitle';
+import SectionTemplate from '../Section/SectionTemplate';
 
 const Projects = () => {
   const { t } = useTranslation();
   return (
     <>
-      <Container
+      <SectionTemplate
         id='projects'
-        maxW='container.xl'
-        display='flex'
-        position='relative'
-        alignItems='center'
-        justifyContent='center'
-        height='100vh'
-        width='100%'
-        flexDirection='column'
+        title={t('MyProjects.1')}
+        subtitle={t('MyLastProjects.1')}
+        sectionNextButton='skills'
       >
-        <SectionHeader title={t('MyProjects.1')} />
-        <SectionSubtitle subtitle={t('MyLastProjects.1')} />
         <Box>
           <List />
         </Box>
         <Box
           position={{ base: 'relative', md: 'relative', lg: 'relative' }}
-          top={{ base: '0', md: '7rem', lg: '7rem' }}
+          top={{ base: '0', md: '7rem', lg: '6rem' }}
         >
           <MoreProjectsButton />
         </Box>
-        <Box mr='2.5rem' display={{ base: 'none', md: 'block', lg: 'block' }}>
-          <SectionNextButton section='skills' />
-        </Box>
-      </Container>
+      </SectionTemplate>
     </>
   );
 };
