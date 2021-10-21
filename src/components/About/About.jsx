2827cb96d834fb7d132f9e5bc364d0786ec1647b
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-21 18:10:49 -0300

    Using SectionTemplate component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,33 +1,20 @@
 import React from 'react';
-import { Container, Box } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
-import SectionNextButton from '../Section/SectionNextButton';
-import SectionHeader from '../Section/SectionHeader';
 import Content from './Content';
-import SectionSubtitle from '../Section/SectionSubtitle';
+import SectionTemplate from '../Section/SectionTemplate';
 
 const About = () => {
   const { t } = useTranslation();
   return (
     <>
-      <Container
+      <SectionTemplate
         id='about'
-        maxW='container.xl'
-        display='flex'
-        position='relative'
-        alignItems='center'
-        justifyContent='center'
-        height='100vh'
-        width='100%'
-        flexDirection='column'
+        title={t('AboutMe.1')}
+        subtitle={t('FrontEndDevJr.1')}
+        sectionNextButton='projects'
       >
-        <SectionHeader title={t('AboutMe.1')} />
-        <SectionSubtitle subtitle={t('FrontEndDevJr.1')} />
         <Content />
-        <Box mr='2.5rem' display={{ base: 'none', md: 'block', lg: 'block' }}>
-          <SectionNextButton section='projects' />
-        </Box>
-      </Container>
+      </SectionTemplate>
     </>
   );
 };
