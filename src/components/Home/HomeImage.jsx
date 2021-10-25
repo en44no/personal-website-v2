f3537f454c3cddc169d48fe71f0e2e06cb14c508
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-25 18:47:13 -0300

    Remove border

--- src/components/Home/HomeImage.jsx
+++ src/components/Home/HomeImage.jsx
@@ -1,34 +1,25 @@
 import React from 'react';
-import { Flex, Image, Box } from '@chakra-ui/react';
+import { Flex, Image } from '@chakra-ui/react';
 import me from '../../assets/Nahuel_Marquez.webp';
 
 const HomeImage = () => {
   return (
     <>
       <Flex ml='5rem'>
-        <Box
-          display='flex'
-          bgGradient='linear(to-r, cyan.400, blue.500, purple.600)'
-          p='0.15rem'
+        <Image
+          loading='lazy'
+          src={me}
+          alt='me, Nahuel Marquez'
           borderRadius='15px'
+          objectFit='fill'
           mt='-3'
-          overflow='hidden'
-          boxSizing='content-box'
+          width='200px'
+          height='280px'
           _hover={{
             transition: 'transform 1s, all 0.2s ease-in-out',
             transform: 'scale(1.02)',
           }}
-        >
-          <Image
-            loading='lazy'
-            src={me}
-            alt='me, Nahuel Marquez'
-            borderRadius='15px'
-            objectFit='fill'
-            width='200px'
-            height='280px'
-          />
-        </Box>
+        />
       </Flex>
     </>
   );
