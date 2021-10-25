760dc282535f9690267b8cec3e4529c4e6924c7d
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-25 19:09:47 -0300

    Removed border and added boxShadow

--- src/components/Projects/ProjectImage.jsx
+++ src/components/Projects/ProjectImage.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
-import { Image, Box } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
 
 const ProjectImage = (props) => {
   return (
     <>
-      <Box
-        display='flex'
-        w={{ base: '237,5px', md: '475px', lg: '475px' }}
-        h={{ base: '112,5px', md: '225px', lg: '225px' }}
-        bgGradient='linear(to-r, cyan.400, blue.500, purple.600)'
-        p='0.15rem'
-        borderRadius='9px'
-        mt='2'
-        overflow='hidden'
-        boxSizing='content-box'
-        _hover={{
-          transition: 'transform 1s, all 0.2s ease-in-out',
-          transform: 'scale(1.02)',
-        }}
-        mb='-2'
-      >
-        <a href={props.demoLink} target='_blank' rel='noreferrer' title='Demo'>
-          <Image
-            loading='lazy'
-            src={props.appImage}
-            alt={props.appTitle}
-            w='475px'
-            h='225px'
-            borderRadius='9px'
-            objectFit='cover'
-          />
-        </a>
-      </Box>
+      <a href={props.demoLink} target='_blank' rel='noreferrer' title='Demo'>
+        <Image
+          loading='lazy'
+          boxShadow='md'
+          mt='2'
+          mb='-2'
+          src={props.appImage}
+          alt={props.appTitle}
+          w='460px'
+          h='212px'
+          borderRadius='9px'
+          objectFit='cover'
+          _hover={{
+            transition: 'transform 1s, all 0.2s ease-in-out',
+            transform: 'scale(1.02)',
+          }}
+        />
+      </a>
     </>
   );
 };
--- src/components/Skills/TechBox.jsx
+++ src/components/Skills/TechBox.jsx
@@ -4,68 +4,55 @@ import { Box, Text } from '@chakra-ui/react';
 const TechBox = (props) => {
   return (
     <>
-      <Box
-        bgGradient='linear(to-r, cyan.400, blue.500, purple.600)'
-        w={{
-          base: '6.5rem',
-          md: '9rem',
-          lg: '9rem',
-        }}
-        p='0.15rem'
-        borderRadius='9px'
-        mt='2'
-        overflow='hidden'
-        boxSizing='content-box'
-        _hover={{
-          transition: 'transform 1s, all 0.2s ease-in-out',
-          transform: 'scale(1.03)',
-        }}
+      <a
+        href={props.link}
+        target='_blank'
+        rel='noreferrer'
+        title={`Link to ${props.title} page`}
       >
-        <a
-          href={props.link}
-          target='_blank'
-          rel='noreferrer'
-          title={`Link to ${props.title} page`}
+        <Box
+          boxShadow='md'
+          w={{
+            base: '6.5rem',
+            md: '9rem',
+            lg: '9rem',
+          }}
+          h='8rem'
+          bg='white'
+          p='4'
+          _hover={{
+            transition: 'transform 1s, all 0.2s ease-in-out',
+            transform: 'scale(1.03)',
+          }}
+          borderRadius='9px'
         >
-          <Box
-            w={{
-              base: '6.5rem',
-              md: '9rem',
-              lg: '9rem',
-            }}
-            h='8rem'
-            bg='white'
-            p='4'
-            borderRadius='9px'
-          >
-            <Box p='2' borderRadius='9px' textAlign='-webkit-center'>
-              <props.techIcon
-                color={props.techColor}
-                fontSize='50px'
-                title={`Link to ${props.title} page`}
-              />
-            </Box>
-            <Text
-              fontWeight='semibold'
-              textAlign='center'
-              fontSize={{
-                base: '13px',
-                md: '',
-                lg: '',
-              }}
-              mt={{
-                base: '0.5rem',
-                md: '',
-                lg: '',
-              }}
-              mb='0'
+          <Box p='2' borderRadius='9px' textAlign='-webkit-center'>
+            <props.techIcon
               color={props.techColor}
-            >
-              {props.title}
-            </Text>
+              fontSize='50px'
+              title={`Link to ${props.title} page`}
+            />
           </Box>
-        </a>
-      </Box>
+          <Text
+            fontWeight='semibold'
+            textAlign='center'
+            fontSize={{
+              base: '13px',
+              md: '',
+              lg: '',
+            }}
+            mt={{
+              base: '0.5rem',
+              md: '',
+              lg: '',
+            }}
+            mb='0'
+            color={props.techColor}
+          >
+            {props.title}
+          </Text>
+        </Box>
+      </a>
     </>
   );
 };
