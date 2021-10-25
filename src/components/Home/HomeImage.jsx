bd75608a0b07269ee9c8d55ddc2a6397af850608
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-25 17:03:29 -0300

    Added border to image

--- src/components/Home/HomeImage.jsx
+++ src/components/Home/HomeImage.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Image, Box } from '@chakra-ui/react';
 import me from '../../assets/Nahuel_Marquez.webp';
 
 const HomeImage = () => {
   return (
     <>
       <Flex ml='5rem'>
-        <Image
-          loading='lazy'
-          src={me}
-          alt='me, Nahuel Marquez'
+        <Box
+          display='flex'
+          bgGradient='linear(to-r, cyan.400, blue.500, purple.600)'
+          p='0.15rem'
           borderRadius='15px'
-          objectFit='fill'
           mt='-3'
-          width='200px'
-          height='280px'
+          overflow='hidden'
+          boxSizing='content-box'
           _hover={{
             transition: 'transform 1s, all 0.2s ease-in-out',
             transform: 'scale(1.02)',
           }}
-        />
+        >
+          <Image
+            loading='lazy'
+            src={me}
+            alt='me, Nahuel Marquez'
+            borderRadius='15px'
+            objectFit='fill'
+            width='200px'
+            height='280px'
+          />
+        </Box>
       </Flex>
     </>
   );
