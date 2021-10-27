2f3e843fe0e8e5390df5f39869fa7c361fa3bf52
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-26 22:48:27 -0300

    Fixed error with Chakra UI

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { ColorModeScript } from '@chakra-ui/color-mode';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
+import './i18next';
+import 'focus-visible/dist/focus-visible';
+import App from './App';
+
+import Loader from './components/Loader';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
+      <Suspense fallback={<Loader />}>
+        <App />
+      </Suspense>
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
