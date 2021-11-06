ac3d3c30b70cc421fae030b062d166407ee918d3
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-11-05 22:05:12 -0300

    Better responsive mode

--- src/components/Skills/GridSkills.jsx
+++ src/components/Skills/GridSkills.jsx
@@ -7,21 +7,30 @@ const GridSkills = (props) => {
   const { t } = useTranslation();
 
   return (
-    <Box mt={{ mobile: sectionText === 'CurrentlySkills.1' ? '2.5rem' : null, tablet: null, laptop: null }}>
+    <Box mt={{
+      mobile: sectionText === 'CurrentlySkills.1' ? '2.5rem' : null, tablet: null, laptop: null, desktop: '-2', largeDesktop: null,
+    }}
+    >
       <Text
         as="h3"
         fontWeight="semibold"
         fontSize={{ mobile: 'md', tablet: 'lg', laptop: 'lg' }}
         textAlign="center"
-        mt={{ mobile: '-3rem', tablet: '-3rem', laptop: '0' }}
-        mb={{ mobile: '0.5rem', tablet: null, laptop: null }}
+        mt={{
+          mobile: '-3rem', tablet: '-3rem', laptop: '0', desktop: '0', largeDesktop: '0',
+        }}
+        mb={{
+          mobile: '0.5rem', tablet: null, laptop: null, desktop: '-1', largeDesktop: null,
+        }}
       >
         {t(sectionText)}
       </Text>
       <Flex
         justifyContent="center"
         flexWrap="wrap"
-        mb={sectionText === 'OtherSkills.1' ? '6rem' : null}
+        mb={{
+          mobile: sectionText === 'OtherSkills.1' ? '6rem' : null, tablet: null, laptop: null, desktop: '-7', largeDesktop: null,
+        }}
       >
         {children}
       </Flex>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -13,7 +13,6 @@ const Skills = () => {
     <>
       <SectionTemplate id="skills" title={t('MySkills.1')}>
         <Box
-          w={{ mobile: '100%', tablet: '90%', laptop: '100%' }}
           pb={{ mobile: '0rem', tablet: '0rem', laptop: '2rem' }}
           position="relative"
           top={{ mobile: '6rem', tablet: '11rem', laptop: '0' }}
@@ -21,9 +20,6 @@ const Skills = () => {
           <CurrentlySkills />
         </Box>
         <Box
-          w={{
-            mobile: '100%', tablet: '90%', laptop: '100%', desktop: '80%', largeDesktop: '80%',
-          }}
           position="relative"
           top={{ mobile: '10rem', tablet: '16rem', laptop: '0' }}
         >
