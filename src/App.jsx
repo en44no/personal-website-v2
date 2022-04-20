7b045542231de0074d94a2c30b565aecd1f3f6e3
NahuelMarquez <nahuel.marquez@gicla.com>
2022-04-20 19:12:24 -0300

    Added React Lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 import smoothscroll from "smoothscroll-polyfill";
-import About from "./components/About/About";
-import Home from "./components/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
-import "./index.css";
-import Projects from "./components/Projects/Projects";
-import GoTop from "./components/GoTop";
-import Skills from "./components/Skills/Skills";
-import MobileNavbar from "./components/Navbar/MobileNavbar";
+import Loader from "./components/Loader";
+const About = React.lazy(() => import("./components/About/About"));
+const Home = React.lazy(() => import("./components/Home/Home"));
+const Navbar = React.lazy(() => import("./components/Navbar/Navbar"));
+const Projects = React.lazy(() => import("./components/Projects/Projects"));
+const GoTop = React.lazy(() => import("./components/GoTop"));
+const Skills = React.lazy(() => import("./components/Skills/Skills"));
+const MobileNavbar = React.lazy(() => import("./components/Navbar/MobileNavbar"));
 
 function App () {
   smoothscroll.polyfill();
 
   return (
     <>
-      <MobileNavbar />
-      <Navbar />
-      <GoTop />
-      <Home />
-      <About />
-      <Projects />
-      <Skills />
+      <Suspense fallback={<Loader />}>
+        <MobileNavbar />
+        <Navbar />
+        <GoTop />
+        <Home />
+        <About />
+        <Projects />
+        <Skills />
+      </Suspense>
     </>
   );
 }
