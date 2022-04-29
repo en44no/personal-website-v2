5333da15998128841f00a98b0dc9775fe056af96
NahuelMarquez <nahuel.marquez@gicla.com>
2022-04-29 00:26:06 -0300

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { Suspense } from "react";
 import smoothscroll from "smoothscroll-polyfill";
 import Loader from "./components/Loader";
+import MobileNavbar from "./components/Navbar/MobileNavbar";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 const About = React.lazy(() => import("./components/About/About"));
-const Home = React.lazy(() => import("./components/Home/Home"));
-const Navbar = React.lazy(() => import("./components/Navbar/Navbar"));
 const Projects = React.lazy(() => import("./components/Projects/Projects"));
 const GoTop = React.lazy(() => import("./components/GoTop"));
 const Skills = React.lazy(() => import("./components/Skills/Skills"));
-const MobileNavbar = React.lazy(() => import("./components/Navbar/MobileNavbar"));
 
 function App () {
   smoothscroll.polyfill();
