4bb3ed0d369dc410ae7dc0680f543db1ccb490dd
NahuelMarquez <nahuel.marquez@gicla.com>
2022-05-04 19:07:47 -0300

    No more lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import React, { Suspense } from "react";
+import React from "react";
 import smoothscroll from "smoothscroll-polyfill";
-import Loader from "./components/Loader";
 import MobileNavbar from "./components/Navbar/MobileNavbar";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-const About = React.lazy(() => import("./components/About/About"));
-const Projects = React.lazy(() => import("./components/Projects/Projects"));
-const GoTop = React.lazy(() => import("./components/GoTop"));
-const Skills = React.lazy(() => import("./components/Skills/Skills"));
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import GoTop from "./components/GoTop";
+import Skills from "./components/Skills/Skills";
 
 function App () {
   smoothscroll.polyfill();
 
   return (
     <>
-      <Suspense fallback={<Loader />}>
-        <MobileNavbar />
-        <Navbar />
-        <GoTop />
-        <Home />
-        <About />
-        <Projects />
-        <Skills />
-      </Suspense>
+      <MobileNavbar />
+      <Navbar />
+      <GoTop />
+      <Home />
+      <About />
+      <Projects />
+      <Skills />
     </>
   );
 }
